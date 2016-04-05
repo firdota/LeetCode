332e753a62140d84beaa77ef4289bf1c7de0e5c5
firdota <lixthu@126.com>
2016-04-05 09:33:51 +0800

    Update 313_Super_Ugly_Number.cpp

--- 313_Super_Ugly_Number.cpp
+++ 313_Super_Ugly_Number.cpp
@@ -5,24 +5,24 @@ class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
         int *uglyNumber = new int[n];
-		int *pointer = new int[primes.size()];
-		memset(pointer,0,primes.size()*sizeof(int));
-		int i=0,j=0,min=0,minIndex=0;
-		uglyNumber[0]=1;
-		for(i=1;i<n;i++){
-			min=INT_MAX;
-			for(j=0;j<primes.size();j++){
-				if(uglyNumber[pointer[j]]*primes[j]<min){
-					minIndex=j;
-					min=uglyNumber[pointer[j]]*primes[j];
-				}
-				else if(uglyNumber[pointer[j]]*primes[j]==min)
-					pointer[j]++;
+	int *pointer = new int[primes.size()];
+	memset(pointer,0,primes.size()*sizeof(int));
+	int i=0,j=0,min=0,minIndex=0;
+	uglyNumber[0]=1;
+	for(i=1;i<n;i++){
+		min=INT_MAX;
+		for(j=0;j<primes.size();j++){
+			if(uglyNumber[pointer[j]]*primes[j]<min){
+				minIndex=j;
+				min=uglyNumber[pointer[j]]*primes[j];
 			}
-			uglyNumber[i]=min;
-			pointer[minIndex]++;
+			else if(uglyNumber[pointer[j]]*primes[j]==min)
+				pointer[j]++;
 		}
-		return uglyNumber[n-1];
+		uglyNumber[i]=min;
+		pointer[minIndex]++;
+	}
+	return uglyNumber[n-1];
     }
 };
 int main(){
