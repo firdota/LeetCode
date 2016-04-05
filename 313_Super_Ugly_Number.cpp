e2f8fdef3156958c67cddc2b713f027cf3970964
firdota <lixthu@126.com>
2016-04-05 09:31:29 +0800

    Create 313_Super_Ugly_Number.cpp
    uglyNumber序列中的所有数都是由前序列中的元素乘以某素数得到，用pointer数组记录第j个素数所能乘的最小值位置（更小的相乘已经出现在序列中了），每生成一个新的元素，需要所有素数乘以对应的pointer数组指示的下标处的元素（uglyNumber数组中）。复杂度为O(nk)。

---
+++ 313_Super_Ugly_Number.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int *uglyNumber = new int[n];
+		int *pointer = new int[primes.size()];
+		memset(pointer,0,primes.size()*sizeof(int));
+		int i=0,j=0,min=0,minIndex=0;
+		uglyNumber[0]=1;
+		for(i=1;i<n;i++){
+			min=INT_MAX;
+			for(j=0;j<primes.size();j++){
+				if(uglyNumber[pointer[j]]*primes[j]<min){
+					minIndex=j;
+					min=uglyNumber[pointer[j]]*primes[j];
+				}
+				else if(uglyNumber[pointer[j]]*primes[j]==min)
+					pointer[j]++;
+			}
+			uglyNumber[i]=min;
+			pointer[minIndex]++;
+		}
+		return uglyNumber[n-1];
+    }
+};
+int main(){
+	Solution s;
+	vector<int> test;
+	test.push_back(2);
+	test.push_back(7);
+	test.push_back(13);
+	test.push_back(19);
+	cout<<s.nthSuperUglyNumber(12,test)<<endl;
+	return 0;
+}
